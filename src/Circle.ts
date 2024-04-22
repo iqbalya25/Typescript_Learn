a2c462ba204f6e3f4c52cd3625737ec40620eee2
iqbalya25 <iqbalya25@gmail.com>
2024-04-22 16:18:12 +0700

    adding Circle function

---
+++ src/Circle.ts
@@ -0,0 +1,9 @@
+const pi : number = Math.PI;
+
+export function calculateCircleCircumference (diameter : number){
+    return pi*diameter;
+}
+
+export function calculateCircleArea (radius : number){
+    return pi*radius**2;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("HEHE")
+import { calculateCircleArea, calculateCircleCircumference } from "./Circle.js";
+// import { findAngle3} from ".";
+
+const diameter: number = 10;
+const radius: number = 5
+
+const Circumference = calculateCircleCircumference (diameter);
+const Area: number = calculateCircleArea (radius);
+
+console.log("Circumference of the rectangle:", Circumference);
+console.log("Area of the rectangle:", Area);
+
+// const angleA: number = 80;
+// const angleB: number = 65;
+
+// const angleC: number = findAngle3 (angleA, angleB);
+// console.log("Angle 3:", angleC);
